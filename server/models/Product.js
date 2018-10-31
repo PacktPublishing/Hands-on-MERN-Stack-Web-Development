2e621d9b06627d0dbbcccc173c1376242d04f61b
Filip Danić <filip@spicefactory.co>
2018-10-31 23:39:12 +0100

    Add code for section 2 video “What’s REST and What’s a RESTful Service?”

---
+++ server/models/Product.js
@@ -0,0 +1,45 @@
+export default class Product {
+  /**
+   *
+   * @param {string} id
+   * @param {string} name
+   * @param {number} price
+   * @param {Array<string>} images
+   */
+  constructor({ id, name, price, images }) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+    this._images = images;
+  }
+
+  /**
+   * @return {string}
+   */
+  getId = () => this._id;
+
+  /**
+   * @return {string}
+   */
+  getName = () => this._name;
+
+  /**
+   * @return {number}
+   */
+  getPrice = () => this._price;
+
+  /**
+   * @return {Array<string>}
+   */
+  getImages = () => this._images;
+
+  /**
+   * @return {{id: string, name: string, price: number, images: Array<string>}}
+   */
+  getData = () => ({
+    id: this._id,
+    name: this._name,
+    price: this._price,
+    images: this._images,
+  });
+}
