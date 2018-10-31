10633a4e17dc50ea062d89670126cd8f78aa31d1
Filip Danić <filip@spicefactory.co>
2018-10-31 22:51:48 +0100

    Add code for section 2 video “Setting up Express”

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const x = { a: 1, b: 2, c: 3 };
-console.log(Object.entries(x));
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/ping', (req, res) => res.send('pong'));
+
+app.get('/heartbeat', (req, res) => res.send({
+  dateTime: new Date().toJSON()
+}));
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
+
