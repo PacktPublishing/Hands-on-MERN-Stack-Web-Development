6873b3a82bedf394060af5532807643445907a1c
Filip Danić <filip@spicefactory.co>
2018-11-02 00:57:02 +0100

    Add code for section 2 video “Setting up Unit Tests with Jest“

---
+++ server/models/tests/Product.test.js
@@ -0,0 +1,11 @@
+import ProductModel from '../Product';
+
+test('It accepts all params and implements all methods', () => {
+  const raw = { id: '1234', name: 'foo', price: 123, images: ['1'] };
+  const product = new ProductModel(raw);
+  expect(product.getId()).toBe(raw.id);
+  expect(product.getName()).toBe(raw.name);
+  expect(product.getPrice()).toBe(raw.price);
+  expect(product.getImages()).toBe(raw.images);
+  expect(product.getData()).toEqual(raw);
+});
---
+++ server/models/tests/User.test.js
@@ -0,0 +1,11 @@
+import UserModel from '../User';
+
+test('It accepts all params and implements all methods', () => {
+  const raw = { id: '1234', username: 'jon', email: 'jon@company.co', role: 'admin' };
+  const user = new UserModel(raw);
+  expect(user.getId()).toBe(raw.id);
+  expect(user.getUsername()).toBe(raw.username);
+  expect(user.getEmail()).toBe(raw.email);
+  expect(user.getRole()).toBe(raw.role);
+  expect(user.getData()).toEqual(raw);
+});
