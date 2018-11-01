3a88dccd1585f9702e44d6c0ff32e8f0f1effaa9
Filip Danić <filip@spicefactory.co>
2018-11-01 18:04:01 +0100

    Add code for section 2 video “Handling GET, POST, PUT, and DELETE Requests”

--- server/index.js
+++ server/index.js
@@ -1,13 +1,45 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import users from './mocks/users';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT;
-
-app.get('/ping', (req, res) => res.send('pong'));
 
 app.get('/heartbeat', (req, res) => res.send({
   dateTime: new Date().toJSON()
 }));
+
+app.get('/v1/users', (req, res) => {
+  res.send(users);
+});
+
+app.get('/v1/users/:id', (req, res) => {
+  res.send(users[0]);
+});
+
+app.post('/v1/users', (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const role = req.body.role;
+  console.log('post: data =>', username, email, role);
+  res.status(200).end();
+});
+
+app.put('/v1/users/:id', (req, res) => {
+  const id = req.params.id;
+  const username = req.body.username;
+  const email = req.body.email;
+  const role = req.body.role;
+  console.log('put: data =>', id, username, email, role);
+  res.status(200).end();
+});
+
+app.delete('/v1/users/:id', (req, res) => {
+  console.log('delete: id =>', req.params.id);
+  res.status(200).end();
+});
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
