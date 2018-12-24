7c32c98de48670312311b689002fd3f57fb8aad8
Filip Danić <filip@spicefactory.co>
2018-12-24 09:30:17 +0100

    Add code for section 5 video “Configuring CORS in Express”

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import logger from './middleware/logger';
 import withAuthenticated from './middleware/withAuthentication';
 import getUserRoutes from './routes/users';
@@ -7,6 +8,7 @@ import getProductRoutes from './routes/products';
 import db from './db';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(withAuthenticated);
